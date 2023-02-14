cdc74b206a59601896aeddd0963f8813663235de
Anton Askling <anton.askling@gmail.com>
2023-02-14 03:28:32 +0100

    add localhost to allowed corse origin

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,7 +67,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('ALLOWED_HOSTS')]
 
-CORS_ALLOWED_ORIGINS = [    "http://localhost:3000",]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1",]
 CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
